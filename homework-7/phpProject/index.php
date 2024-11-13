a23e50fb50f6b407d9e97d1a4b31051a3ca210ab
Dante Piedalue <132118768+Dan-teaa@users.noreply.github.com>
2024-11-13 15:26:52 -0500

    added in-class-10, and an edit to HW7 index.php

--- homework-7/phpProject/index.php
+++ homework-7/phpProject/index.php
@@ -2,19 +2,19 @@
 
 require 'userController.php';
 
-// Instantiate UserController
+//Instantiate 
 $userController = new UserController();
 
-// Use the `url` query parameter from .htaccess for routing
-$requestURI = isset($_GET['url']) ? $_GET['url'] : '';
+//Superglobal for the request method
+$requestURI = $_SERVER['REQUEST_URI'];
 
-// Route based on the `url` parameter
 if ($requestURI == '/users') {
-    // Call the method to return JSON data
+    //Indicates to controller to request json data
     $userController->getUserData();
 } else {
-    // Call the method to return the HTML view
     $userController->getUserView();
 }
+
+
 
 ?>
